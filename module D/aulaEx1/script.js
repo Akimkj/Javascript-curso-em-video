c4dd23e23508fb46ef860dfd8c1c1a2513314c2f
Mika <kakamarques.0202@gmail.com>
2025-03-04 19:26:16 -0300

    exercicio horario dia

--- module D/aulaEx1/script.js
+++ module D/aulaEx1/script.js
@@ -1,10 +1,21 @@
 var agora = new Date()
 var hora = agora.getHours()
 
-var corDeFundoMain = window.document.getElementById("imagemdoDia")
+var fundoBody = window.document.body
 var textoHoras = window.document.getElementById("mensagemHoraDoDia")
+var imagem = window.document.getElementById("imagem")
 
 textoHoras.innerText = `Agora são ${hora} horas`
-if (hora >= 0 && hora <= 12) {
-    window.document.corDeFundoMain.style.backgroundImage = URL("Amanhecer01.jpg")
+
+if (hora > 0 && hora < 12) {
+    imagem.src = 'Amanhecer01.jpg'
+    fundoBody.style.backgroundColor = "rgb(247, 229, 128)"
+}
+if (hora >= 12 && hora < 18) {
+    imagem.src = 'tarde.jpg'
+    fundoBody.style.backgroundColor = "rgb(245, 179, 93)"
+}
+if (hora >= 18) {
+    imagem.src = 'noite.jpeg'
+    fundoBody.style.backgroundColor = "rgb(73, 73, 73)"
 }
